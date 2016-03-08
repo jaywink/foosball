9cffb8243df4f0b5e35f31a0e35e872dc62d9e07
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-03-08 22:42:15 +0200

    Add Game to Admin UI

--- foosball/games/admin.py
+++ foosball/games/admin.py
@@ -1,3 +1,57 @@
+from django import forms
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
-# Register your models here.
+from .models import Game, Team
+
+
+class TeamForm(forms.ModelForm):
+    model = Team
+
+    def has_changed(self):
+        # force Teams to be saved also when they aren't modified,
+        # prevents Games with less than two Teams
+        return True
+
+
+class TeamInlineFormSet(forms.models.BaseInlineFormSet):
+    def clean(self):
+        teams = []
+        for form in self.forms:
+            players = form.cleaned_data.get('players')
+            if not players:
+                continue  # 'This field is required' error is shown here anyway
+            if players.count() > 2:
+                form.add_error('players', _('Maximum number of players is two.'))
+            teams.append(players.all())
+        if len(teams) == 2 and not set(teams[0]).isdisjoint(teams[1]):
+            self.forms[0].add_error('players', _('Teams contain same players.'))
+
+
+class TeamInline(admin.TabularInline):
+    model = Team
+    form = TeamForm
+    formset = TeamInlineFormSet
+    extra = 2
+    max_num = 2
+
+    class Media:
+        css = {
+            'all': ('css/admin_hide_team_str.css',)
+        }
+
+    def has_delete_permission(self, request, obj=None):
+        # a Game should always have both teams
+        return False
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'played_at', 'created_by', 'modified_by', 'created', 'modified')
+    inlines = (TeamInline,)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        obj.save()
